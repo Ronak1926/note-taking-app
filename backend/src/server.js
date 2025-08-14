446e6495b16f81235d0bf2111e03be0e6a24a3bd
Ronak <ronaksojitra1926@gmail.com>
2025-08-14 23:19:34 +0530

    model created successfully

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from "./config/db.js";
-const app = express();
+import dotenv from "dotenv"
 
+dotenv.config()
+const app = express();
+const PORT = process.env.PORT || 5001
 connectDB();
 app.use("/api/notes", notesRoutes)
-app.listen(5001, () => {
-    console.log("Server Started on Port : 5001")
+app.listen(PORT, () => {
+    console.log(`Server Started on Port : ${PORT}`)
 })
 
 
