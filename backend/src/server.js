ff32ae794b0c0b3269f5964e68259dfe6f7562fb
Ronak <ronaksojitra1926@gmail.com>
2025-08-15 00:39:37 +0530

    little fix in connecting the mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,12 +7,13 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5001
-connectDB();
 app.use(express.json())
 app.use(rateLimiter)
 app.use("/api/notes", notesRoutes)
-app.listen(PORT, () => {
-    console.log(`Server Started on Port : ${PORT}`)
+connectDB().then(() =>{
+    app.listen(PORT, () => {
+        console.log(`Server Started on Port : ${PORT}`)
+    })
 })
 
 
